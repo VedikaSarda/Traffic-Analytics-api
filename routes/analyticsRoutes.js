b3aa02863d74d25ac3a963e5f9ed314ab491672a
Vedika Sarda <vedikasarda5@gmail.com>
2025-06-19 19:00:52 +0530

    Update analyticsRoutes.js

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const analytics = require('../controllers/analyticsController');
 const auth = require('../middleware/auth');
 
-router.get('/summary', analytics.getSummary);
-router.get('/pages', analytics.getPages);
-router.get('/referrers', analytics.getReferrers);
-router.get('/export', analytics.exportData);
-router.get('/timeline', analytics.getTimeline);
-router.get('/devices', analytics.getDevices);
-router.get('/geo', analytics.getGeo);
+router.get('/summary',auth, analytics.getSummary);
+router.get('/pages',auth, analytics.getPages);
+router.get('/referrers',auth, analytics.getReferrers);
+router.get('/export',auth, analytics.exportData);
+router.get('/timeline',auth, analytics.getTimeline);
+router.get('/devices',auth, analytics.getDevices);
+router.get('/geo',auth, analytics.getGeo);
 module.exports = router;
 
-
+// File: middleware/rateLimiter.js
+//const rateLimit = require("express-rate-limit");
 // console.log('auth is:', typeof auth); // should be 'function'
 // console.log('analytics.getSummary is:', typeof analytics.getSummary); // should be 'function'
 // console.log('analytics.getPages is:', typeof analytics.getPages); // should be 'function'
